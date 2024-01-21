e63753acd327939aa890e48ca80da5eaf444d90a
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-01-21 01:04:24 -0500

    sqlalchemy db created

--- python/main.py
+++ python/main.py
@@ -1,9 +1,13 @@
 from openai import OpenAI
 import os
-import pandas as pd
 from dotenv import load_dotenv
+import pandas as pd
+from sqlalchemy import create_engine
+from sqlalchemy import text
+from watcher import *
 
 load_dotenv()
+# watcher.watch()
 
 # set api key on openai 
 
@@ -29,3 +33,21 @@ client = OpenAI(
 data_frame = pd.read_csv("sales_data_sample.csv")
 print(data_frame)
 print(data_frame.groupby('QTR_ID').sum()['SALES'])
+
+# TEMP DB in RAM; can connect to other db instead later
+temp_db = create_engine('sqlite:///:memory:', echo=True)
+
+
+# PUSH Pandas Data Frame --> TEMP DB
+# name= db table name;  
+data = data_frame.to_sql(name='Sales', con=temp_db)
+# print(data)
+
+# Perform SQL queries on TEMP DB with Pandas DF
+with temp_db.connect() as conn:
+    # makes the connection
+    # runs code block
+    # result = conn.execute(text("SELECT * from Sales"))
+    result = conn.execute(text("SELECT SUM(SALES) From Sales"))
+    # auto close connection
+    result.all()
